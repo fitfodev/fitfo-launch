e83d2100201a3b94a765b1445a08e6fc733dfe8a
orlandokennycs <kenny@cs.fsu.edu>
2022-06-04 15:06:28 -0400

    update

--- webapp/src/Home.js
+++ webapp/src/Home.js
@@ -86,7 +86,7 @@ class Home extends React.Component {
                     }
                   }}/>
               </InputGroup>
-              <ToggleButton variant="light" type="submit" className="boldBtn" onClick={this.checkPassword}>
+              <ToggleButton variant="light" type="submit" className="boldBtn" onClick={e => (this.checkPassword(this.state.passwordEntered))}>
                 submit
               </ToggleButton>
             </div>
@@ -94,7 +94,9 @@ class Home extends React.Component {
         </div>
         
       </div>
-      <img src={icon} className='footer' alt='footer' ></img>   
+      <img src={icon} className='footer' alt='footer' >
+        
+        </img>   
 
       <div className='front' style={{visibility: this.state.rightPassword === true ? 'visible' : 'hidden', opacity: this.state.rightPassword === true ? '1':'0', transition: this.state.rightPassword === true ? 'none':'visibility 0s 1s, opacity 1s linear' }}>
         <div id='wrapper'>
@@ -106,29 +108,39 @@ class Home extends React.Component {
             
         
               {this.state.rightPassword === true && 
-              <div>
+              <div className='verticalish-center'>
                 <b>
-              <h2>CONGRATS, YOU'RE IN</h2>
-              <h2>JUNE 22, 2022</h2>
+              <h2 className='firstLine'>CONGRATS, YOU'RE IN<br></br>JUNE 22, 2022</h2>
+             
               <div className='container'>
                 <div className='divdiv'>
-                  <h2 style={{marginTop: '.5em'}}>2103 FAULKNER RD</h2>
+                  <h2 style={{marginTop: '1em'}} className='firstLine'>2103 FAULKNER RD</h2>
                   <img src={square} className='square'></img>
-                  <h2>7PM-10PM</h2>
+                  <h2 className='firstLine'>7PM-10PM</h2>
                 </div>
                 <div className='divdiv'>
-                  <h2 style={{marginTop: '.5em'}}>RULES</h2>
-                  <h3>NO ENTEROUGES</h3>
-                  <h3>BRING A STICKER</h3>
-                  <h3>ANOTHER RULE</h3>
-                  <h3>BE GENUINE</h3>
-                  <h3>NETWORK</h3>
-                  <h3>ENSURE FUNNESS</h3>
-                  <h3>NUMBER SEVEN</h3>
-                </div>
-                <ToggleButton variant="light" type="submit" className='customBtn' onClick={this.checkPassword}>
+                  <h2 style={{marginTop: '1em'}}>RULES</h2>
+                  <br></br>
+                  <div className='verticalrules-center'>
+                    <div>
+                      <div className='divdiv2'>
+                        <h3>NO ENTEROUGES</h3>
+                        <h3>BRING A STICKER</h3>
+                        <h3>ANOTHER RULE</h3>
+                      </div>
+                      <div className='divdiv2'>
+                        <h3>NETWORK</h3>
+                        <h3>ENSURE FUNNESS</h3>
+                        <h3>NUMBER SEVEN</h3>
+                      </div>
+                  </div>
+                  <ToggleButton variant="light" type="submit" className='customBtn' onClick={e => (alert("hello"))} style={{marginTop: '2em'}}>
                     RSVP
                   </ToggleButton>
+                  </div>
+                  
+                </div>
+                
                   
               </div>
               </b>
